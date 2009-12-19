e812f2876f03b0ed7af920ac0e79e2f5c8a90326
Charles Childers <crc.platypus@gmail.com>
2009-12-19 16:54:10 -0500

    minor tweaks

--- apps/rem.f
+++ apps/rem.f
@@ -25,41 +25,42 @@ stub rem
 {
   stub r      ( refresh )
   stub q      ( input   )
-  stub p      ( prev    )
-  stub n      ( next    )
   stub v      ( view    )
   variable l  ( line    )
   variable c  ( column  )
 
-  here is p ] blk -- ;
-  here is n ] blk ++ ;
+  : p ( - ) blk -- ;  ( previous )
+  : n ( - ) blk ++ ;  ( next     )
 
-  : up l -- ; : dn l ++ ;
-  : lt c -- ; : rt c ++ ;
+  : up ( - ) l -- ;
+  : dn ( - ) l ++ ;
+  : lt ( - ) c -- ;
+  : rt ( - ) c ++ ;
 
-  : match: ` char: ` over ` =if ; immediate
+  : match: ( "- )
+    ` char: ` over ` =if ; immediate
   : bounds ( - )
     c @ -1 =if 63 c ! dn then
     c @ 64 =if  0 c ! up then
     l @ -1 =if  0 l ! p then
     l @  8 =if  0 l ! n then
     blk @ -1 =if 0 s then
+    blk @ #-blocks @ >if blk -- then
   ;
 
   {
-    : pos  ( -cl)  c @ l @ ;
-    : get  ( cl-a) (line) + ;
-    : addr ( -a )  pos get ;
-    : va   ( a-va) dup @ swap ;
-    : c!   ( a-)   char: * swap ! ;
-    : show ( va- ) dup c! (v) ! ;
-    : wr   ( n- ) bounds addr ! ;
+    : pos  ( -cl  )  c @ l @ ;
+    : get  ( cl-a )  (line) + ;
+    : addr ( -a   )  pos get ;
+    : va   ( a-va )  dup @ swap ;
+    : c!   ( a-   )  char: * swap ! ;
+    : show ( va-  )  dup c! (v) ! ;
+    : wr   ( n-   )  bounds addr ! ;
     here is r ] v cr pos . . ;
     here is v ] ( - ) addr va show ;
     here is q ] ( n-n )
       key
       dup 27 =if ;then
-      dup 10 =if ;then
       dup 8  =if lt 32 wr r q ;then
       wr rt r q ;
   }
